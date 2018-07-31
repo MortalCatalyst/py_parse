3252e254aa5da35ac5dfeee4ce7863606d9528aa
mortalcatalyst <flebber.crue@gmail.com>
2018-07-31 22:02:09 +1000

    processing attrs

--- parse.py
+++ parse.py
@@ -4,55 +4,24 @@ from bs4 import BeautifulSoup
 
 with open('20180728RHIL0.xml', 'rb') as fp:
     soup = BeautifulSoup(fp, "xml")
-    # data = f.read()
 
     for cond in soup.find_all('condition'):
         cond.extract()
 
-    print(soup.select('race.id'))
-    # print(soup)
+    races = soup.findAll('race')
 
-    # items = [i for s in ['race.id', 'race.name'] for i in soup.select(s)]
-    # print(items)
-    # parser = etree.XMLParser(remove_comments=True)
-    # tree = etree.parse(data, parser=parser)
-    # tree = etree.parse(data)
-    # root = etree.fromstring(data)
-    # children = list(root)
-    # # print(root[0].child.getnext())
-    # for child in root:
-    #     print(root[0].child.getnext())
-    # samples = root.xpath('//meeting/race')
-    # child_dicts = collections.namedtuple(children)
-    # print(child_dicts)
+    race_keys = ['id', 'number', 'nomnumber', 'division', 'name', 'mediumname', 'stage', 'distance', 'minweight', 'raisedweight', 'class', 'age', 'grade',
+                 'weightcondition', 'owner', 'totalprize', 'first', 'second', 'third', 'time', 'trackcondition', 'timingmethod', 'fastesttime', 'sectionaltime']
 
+    for race_id in races:
+        for k in race_keys:
+            print("{0} \t:: {1}".format(k, race_id[k]))
+            # print("{0} :: {1}".format(race_id.attrs[k], race_id.attrs[v]))
+
+    race_attrs = []
+    for attr in soup.race.attrs:
+        race_attrs.append(attr)
+
+    # Relevant for processing without comments
     # parser = objectify.makeparser(remove_comments=True)
     # tree = objectify.parse(f, parser=parser)
-
-    # # print(tree)
-    # def recursive_dict(element):
-    #     return element.tag, dict(map(recursive_dict, element)) or element.text
-
-    # data_dict = recursive_dict(tree.getroot())
-
-    # print(data_dict)
-    # races = soup.findAll("race").findChildren()
-    # races = soup.findAll(lambda tag: tag.name ==
-    #                      "race" and tag.name == "nomination")
-    # races = soup.findAll(["race", "nomination"])
-    # print(races[0])
-    # a = []
-    # for item in soup.findAll("race"):
-    #     if item.find("condition") not in item:
-    #         a.append(item)
-    #         continue
-
-    # # print(a)
-    # no_condition = list(
-    #     filter(lambda x: x == "condition", races))
-    # print(no_condition)
-
-    # outfile = [cond.extract() for cond in soup.find_all("condition")]
-    # for cond in soup.find_all('condition'):
-    #     print(cond.extract())
-    # print(outfile)
